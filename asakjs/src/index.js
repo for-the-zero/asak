57b24d270b0408e26fda6836b786f6372660e4b2
for-the-zero <ourmicroid@outlook.com>
2025-05-11 14:59:55 +0800

    js: all methods

--- asakjs/src/index.js
+++ asakjs/src/index.js
@@ -1,3 +1,5 @@
+const { Configuration, OpenAIApi } = require("openai");
+
 export class asak {
     #config = {providers: {}, models: []};
     #records = [];
@@ -76,10 +78,8 @@ export class asak {
                         this.#records[i].limit_m === records[i].limit_m &&
                         this.#records[i].limit_d === records[i].limit_d
                     ){
-                        if(records[i].m.length > 0 && records[i].d.length > 0){
-                            new_records[i].m.push(...records[i].m);
-                            new_records[i].d.push(...records[i].d);
-                        };
+                        new_records[i].m.push(...records[i].m);
+                        new_records[i].d.push(...records[i].d);
                     } else {
                         throw new Error('The format of your records is not correct or rpm/rpd is not equal to the models');
                     };
@@ -93,25 +93,29 @@ export class asak {
     };
 
     #is_model_avaliable(i){
-        this.#recorder_ognz();
         if(this.#records[i].m.length < this.#records[i].limit_m && this.#records[i].d.length < this.#records[i].limit_d){
-            let spare_m = (this.#records[i].limit_m - this.#records[i].m.length) / this.#records[i].limit_m;
-            let spare_d = (this.#records[i].limit_d - this.#records[i].d.length) / this.#records[i].limit_d;
-            return {usable: true, spare: Math.min(spare_m, spare_d)};
-        } else {
-            return {usable: false, spare: null};
-        };
+            return true;
+        } else {return false;};
+    };
+    #model_availability(i){
+        let m_avblty = (this.#records[i].limit_m - this.#records[i].m.length) / this.#records[i].limit_m;
+        let d_avblty = (this.#records[i].limit_d - this.#records[i].d.length) / this.#records[i].limit_d;
+        return Math.min(m_avblty, d_avblty);
     };
     get_model(mode, filter=(i,m)=>{return true;} ){
+        this.#recorder_ognz();
         let preparing_models = [];
         if(typeof filter === 'function'){
             for(let i = 0; i < this.#config.models.length; i++){
-                if(filter(i,this.#config.models[i] && this.#is_model_avaliable(i).usable)){
+                if(filter(i,this.#config.models[i]) && this.#is_model_avaliable(i)){
                     preparing_models.push(i);
                 };
             };
         } else {
             throw new Error('Filter param is not a function');
+        };
+        if(preparing_models.length === 0){
+            throw new Error('No model is avaliable');
         };
         let selected_model;
         switch(mode){
@@ -119,7 +123,7 @@ export class asak {
                 selected_model = Math.min(...preparing_models);
                 break;
             case 'avaliable':
-                //TODO:
+                selected_model = preparing_models.sort((a,b)=>{return this.#model_availability(b) - this.#model_availability(a);})[0];
                 break;
             case 'random':
                 selected_model = preparing_models[Math.floor(Math.random() * preparing_models.length)];
@@ -127,8 +131,40 @@ export class asak {
             default:
                 throw new Error('Mode param is not valid');
         };
+        return {
+            "provider": this.#config.models[selected_model].provider,
+            "base_url": this.#config.providers[this.#config.models[selected_model].provider].base_url,
+            "key": this.#config.providers[this.#config.models[selected_model].provider].key,
+            "model": this.#config.models[selected_model].model,
+        };
     };
-    request(mode, filter, messages){
-        //TODO:
+    async request(mode, filter, messages){
+        let selected_model = this.get_model(mode, filter);
+        let openai_api = new OpenAIApi(new Configuration({
+            apiKey: selected_model.key,
+            baseURL: selected_model.base_url,
+        }));
+        const stream = await openai_api.createChatCompletion({
+            model: selected_model.model,
+            messages: messages,
+            stream: true
+        });
+        async function* deltaGenerator() {
+            for await (const chunk of stream) {
+                const content = chunk.choices?.[0]?.delta?.content;
+                if (content !== undefined) {
+                    yield content;
+                };
+            };
+        };
+        return {
+            provider: selected_model.provider,
+            base_url: selected_model.base_url,
+            key: selected_model.key,
+            model: selected_model.model,
+            delta: {
+                [Symbol.asyncIterator]: () => deltaGenerator()[Symbol.asyncIterator]()
+            }
+        };
     };
 };
